e57139cb1331b25d71c2d1639970ca6f78ec2b06
Viktor1504 <viktordenisevich6@gmail.com>
2024-04-25 00:08:38 +0300

    HW12_NEW

--- src/s2-homeworks/hw12/HW12.tsx
+++ src/s2-homeworks/hw12/HW12.tsx
@@ -7,9 +7,9 @@ import {AppStoreType} from '../hw10/bll/store';
 import {changeThemeId} from './bll/themeReducer';
 
 const themes = [
-    {id: 1, value: 'Light'},
-    {id: 2, value: 'Blue'},
-    {id: 3, value: 'Dark'},
+    {id: 1, value: 'light'},
+    {id: 2, value: 'blue'},
+    {id: 3, value: 'dark'},
 ]
 
 const HW12 = () => {
